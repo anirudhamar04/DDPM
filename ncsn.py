926581fa3a745b88794092948fa4b6ce332b0bd7
anirudh <anirudhamar04@gmail.com>
2025-11-15 15:02:04 +0530

    changed noise steps

--- ncsn.py
+++ ncsn.py
@@ -20,7 +20,7 @@ class NCSN:
     """
     
     def __init__(self, sigma_min=0.01, sigma_max=50.0, num_noise_levels=50, 
-                 img_size=64, device="cuda", langevin_steps=20, langevin_step_size=5e-5):
+                 img_size=64, device="cuda", langevin_steps=100, langevin_step_size=5e-5):
         """
         Args:
             sigma_min: Minimum noise level (finest scale)
@@ -275,7 +275,7 @@ def train(args):
     device = args.device if torch.cuda.is_available() else "cpu"
     dataloader = get_data(args)
     model = UNetNCSN(device=device).to(device)
-    optimizer = optim.AdamW(model.parameters(), lr=args.lr)
+    optimizer = optim.AdamW(model.parameters(), lr=args.lr,betas=(0.9,0.999))
     mse = nn.MSELoss()
     ncsn = NCSN(
         sigma_min=args.sigma_min,
